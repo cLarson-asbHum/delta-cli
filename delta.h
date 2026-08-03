e724447638dd495ea3ac9fe55514e54af4ea8929
Connor Larson <absolutelynotahuman@gmail.com>
2026-08-03 12:59:38 -0500

    Delta header file

---
+++ delta.h
@@ -0,0 +1,123 @@
+// delta.h
+//
+// Computes and patches the smallest string-to-string correction using
+// in terms of moves and additions.
+//
+// The algorithms for computing file deltas are derived from the following paper:
+// Tichy, Walter F., "The String-to-String Correction Problem with Block Moves"
+// (1983). Department of Computer Science Technical Reports. Paper 378.
+// https://docs.lib.purdue.edu/cstech/378
+
+#ifndef DELTA_H
+#define DELTA_H
+
+#include <stdio.h>
+
+// Indicates that a part of the previous will be moved to the specified index.
+// Moves can overlap with one another, which allows for deletions.
+struct MoveCommand
+{
+        unsigned long prevIndex; // Where the substring was in the previousSrc string
+        unsigned long curIndex;  // Where the substring is in the currentSrc string
+        unsigned long len;       // How long the substring
+};
+
+// Indicates that a specific symbol present in current was not present in
+// previous.
+struct AddCommand
+{
+        unsigned char symbol;
+        unsigned long curIndex; // Where the symbol is in the currentSrc string
+};
+
+enum CommandType
+{
+        MOVE_COMMAND,
+        ADD_COMMAND
+};
+
+// A command of runtime-determined type.
+struct Command
+{
+        enum CommandType type;
+        union
+        {
+                struct MoveCommand *move;
+                struct AddCommand *add;
+        } command;
+};
+
+// Computes the largest block move, as described by procedure L in Tichy (page 8).
+// If there is not enough data, this returns `NULL` from stdio.
+//
+// previousSrc  - pointer to the first byte the previous src
+// prevStart    - Index to start at in previous src
+// prevLen      - length of previous src
+// currentSrc   - pointer to the first byte of the prefix (start of L in Tichy)
+// curMaxCount  - maximum length of the prefix
+struct Command *nextLargestMove(const unsigned char *previousSrc,
+                                unsigned long prevStart, unsigned long prevLen,
+                                const unsigned char *currentSrc, 
+                                unsigned long curMaxCount);
+
+// Performs the operation specified by the command, outputing in the specified
+// buffer. This returns the number of bytes written as a result of command. If
+// the return value is not equal to `patchSizeOf(command)`, an error has occurred
+// (most likely the end of a buffer, either input or output, was unexpectedly 
+// reached).
+//
+// previousSrc - pointer to the first byte of the buffer onto which the patch 
+//               command is applied
+// prevLen     - length of previous src, in number of chars
+// out     - output destination of the command. Existing bytes in the 
+//           command's domain will be overridden; all others are unmodified
+// outLen  - length of out, in number of chars
+// command - the patch command to apply and write to out
+int patchCommand(const unsigned char *previousSrc, unsigned long prevLen,
+                 unsigned char *out, unsigned long outLen, 
+                 const struct Command *command);
+
+// Determines how many bytes this command changes in the output. For adds, it's
+// only one because only 1 symbol is added; for moves, it's equal to the `len` 
+// struct member.
+int patchSizeOf(const struct Command *command);
+
+// Determines how many bytes are necessary to serialize the given command. 
+int serialSizeOf(const struct Command *command);
+
+/* Writes the command as a series of bytes to the specified buffer, starting at
+ * index i. This returns the number of bytes which were successfully written,
+ * which is less than or equal to the serialization size of the given command.
+ * If the number of bytes written is less than `serialSizeOf(command)`, an
+ * error occurred.
+ *
+ * buf     - pointer to first unsigned char in the to-serialize output
+ * bufSize - length of the buffer array, in number of unsigned chars
+ * i       - the first index to write to when serializing the command
+ * command - what to serialize
+ */
+int serializeCommand(unsigned char *buf, unsigned long bufSize,
+                     unsigned long i, const struct Command *command);
+
+/* Synthesizes a command from the given buffer, starting at the given index. This
+ * is most useful when reading from a file. Parameter `destination` is used as
+ * the result of deserialization.
+ * 
+ * This procedure assumes that the index `i` is, in fact, the first byte of a 
+ * command; if it isn't (e.g. it's a part of a file's meta data), garbage data 
+ * will be written to the command destination without returning any error 
+ * (assuming no error described below occurs).
+ *
+ * When successful, the function returns `EXIT_SUCCESS` from stdio.h. If anything
+ * else is returned, an error has occurred (e.g. end of the buffer was reached),
+ * the destination pointer is in an undefined state.
+ *
+ * buf     - pointer to first unsigned char in the serialized source
+ * bufSize - length of the buffer array, in number of unsigned chars
+ * i       - the first index to read when deserializing the command
+ * destination - where the deserialized data will be written
+ */
+int *deserializeCommand(const unsigned char *buf, unsigned long bufsize,
+                        unsigned long i, struct Command *destination);
+
+#endif
