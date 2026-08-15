4bcf47a71dc5b2c42a583d7036a4f712bb744c59
Connor Larson <absolutelynotahuman@gmail.com>
2026-08-14 20:19:08 -0500

    Hash generation algorithms

--- src/hash.c
+++ src/hash.c
@@ -1,9 +1,9 @@
 // This code is based off of the SHA-256 specifications from RFC-6234.
 // It is not derived from the C source code present in the document.
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "hash.h"
-
-#define NULL ((void *)0)
 
 uint64_t solveK(uint64_t msgBytes)
 {
@@ -20,9 +20,6 @@ uint64_t solveK(uint64_t msgBytes)
         return kBits;
 }
 
-// Gets the length that a message of `msgBytes` octets would be padded to for 
-// SHA-256 hashing. The return value is in bytes, not bits! If msgBytes is too large 
-// (>= 2**61), this returns 0; this is the only error that can occur
 uint64_t paddedLen(uint64_t msgBytes) 
 {
         if (msgBytes >= (1uLL << 61)) {
@@ -58,26 +55,201 @@ uint64_t padMsg(uint8_t *outBuf, uint64_t outBytes, const uint8_t *msg,
 
         if (finalLenBytes > outBytes) {
                 // Our padded message won't fit
-                printf("Not enough room. Need %llu; got %llu\n", finalLenBytes, outBytes);
                 return 0;
         }
 
-        // Padding with the length of the message, in little endian
-        for (uint8_t i = 0; i < 8; i++) {
-                outBuf[i] = (msgBytes & (0xff << (8 * i))) >> (8 * i);
+        // Writing the message as-is first
+        for (uint64_t i = 0; i < msgBytes; i++) {
+                outBuf[i] = msg[i];
         }
 
-        // Padding a whole bunch of zeros, and the 1 at the end
-        for (uint64_t i = 8; i < paddingBytes; i++) {
+        // Appending a 1 bit to the end of the message (equivalent to 0x80 after it)
+        outBuf[msgBytes] = 0x80u;
+
+        // Writing the myriad of zeros
+        for (uint64_t i = msgBytes + 1; i < finalLenBytes - 8; i++) {
                 outBuf[i] = 0;
         }
 
-        outBuf[paddingBytes - 1] = 0x80u; // 0b1000_0000
+        // Writing the length in big endian
+        for (uint8_t i = finalLenBytes - 8; i < finalLenBytes; i++) {
+                const uint8_t shift = 8 * (i - finalLenBytes + 8);
+                outBuf[i] = ((8 * msgBytes) & (0xff << (56 - shift))) >> (56 - shift);
+        }
+        return finalLenBytes;
+}
 
-        // Appending our actual message
-        for (uint64_t i = paddingBytes; i < finalLenBytes; i++) {
-                outBuf[i] = msg[i - paddingBytes];
+uint32_t rotr(uint32_t x, uint8_t n) 
+{
+        return (x >> n) | ((((1 << n) - 1) & x) << (32 - n));
+}
+
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z) 
+{
+        return (x & y) ^ (~x & z);
+}
+
+uint32_t maj(uint32_t x, uint32_t y, uint32_t z) 
+{
+        return (x & y) ^ (x & z) ^ (y & z);
+}
+
+uint32_t bsig0(uint32_t x) 
+{
+        return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
+}
+
+uint32_t bsig1(uint32_t x) 
+{
+        return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
+}
+
+uint32_t ssig0(uint32_t x) 
+{
+        return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
+}
+
+uint32_t ssig1(uint32_t x) 
+{
+        return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
+}
+
+// Arbitrary constants described in RFC 6234 section 5.1
+const uint32_t K[] = PRIME_CBRTS;
+
+// Reads the i-th 32 bit unsigned big-endian integer stored in the buffer. 
+// Essentially, this converts from a big endian integer into a system-endianness
+// integer.
+uint32_t readBigEnd(const uint8_t *buf, uint64_t start) 
+{
+        uint32_t total = 0x00000000;
+        for (uint8_t i = 0; i < 4; i++) {
+                total = total | (buf[4 * start + i] << (24 - 8 * i));
+        }
+        return total;
+}
+
+void writeBigEnd(uint8_t *buf, uint64_t start, uint32_t val) 
+{
+        for (uint8_t i = 0; i < 4; i++) {
+                buf[4 * start + i] = (val & (0xff << (24 - 8 * i))) >> (24 - 8 * i);
+        }
+}
+
+uint8_t computeBlockHash(union Sha256 *dest, const uint8_t block[64]) 
+{
+        if (dest == NULL || block == NULL) {
+                return 0;
         }
 
-        return finalLenBytes;
+        // Preparing the message schedule
+        uint32_t w[64];
+        for (uint8_t i = 0; i < 16; i++) {
+                w[i] = readBigEnd(block, i);
+        }
+
+        for(uint8_t i = 16; i < 64; i++) {
+                w[i] = ssig1(w[i - 2]) + w[i - 7] + ssig0(w[i - 15]) + w[i - 16];
+        }
+
+        // Initialize the working variables
+        uint32_t a = readBigEnd(dest->bytes, 0);
+        uint32_t b = readBigEnd(dest->bytes, 1);
+        uint32_t c = readBigEnd(dest->bytes, 2);
+        uint32_t d = readBigEnd(dest->bytes, 3);
+        uint32_t e = readBigEnd(dest->bytes, 4);
+        uint32_t f = readBigEnd(dest->bytes, 5);
+        uint32_t g = readBigEnd(dest->bytes, 6);
+        uint32_t h = readBigEnd(dest->bytes, 7);
+
+        // Main hash computation
+        for (uint8_t i = 0; i < 64; i++) {
+                const uint32_t tmp1 = h + bsig1(e) + ch(e, f, g) + K[i] + w[i];
+                const uint32_t tmp2 = bsig0(a) + maj(a, b, c);
+                h = g;
+                g = f;
+                f = e;
+                e = d + tmp1;
+                d = c;
+                c = b;
+                b = a;
+                a = tmp1 + tmp2;
+        }
+
+        // Finalizing the intermediate hash value (the result for this function):
+        writeBigEnd(dest->bytes, 0, a + readBigEnd(dest->bytes, 0));
+        writeBigEnd(dest->bytes, 1, b + readBigEnd(dest->bytes, 1));
+        writeBigEnd(dest->bytes, 2, c + readBigEnd(dest->bytes, 2));
+        writeBigEnd(dest->bytes, 3, d + readBigEnd(dest->bytes, 3));
+        writeBigEnd(dest->bytes, 4, e + readBigEnd(dest->bytes, 4));
+        writeBigEnd(dest->bytes, 5, f + readBigEnd(dest->bytes, 5));
+        writeBigEnd(dest->bytes, 6, g + readBigEnd(dest->bytes, 6));
+        writeBigEnd(dest->bytes, 7, h + readBigEnd(dest->bytes, 7));
+        return 1;
+}
+
+uint8_t initSha256(union Sha256 *dest) 
+{
+        if (dest == NULL) {
+                return 0;
+        }
+
+        writeBigEnd(dest->bytes,0, (uint32_t) 0x6a09e667u);
+        writeBigEnd(dest->bytes,1, (uint32_t) 0xbb67ae85u);
+        writeBigEnd(dest->bytes,2, (uint32_t) 0x3c6ef372u);
+        writeBigEnd(dest->bytes,3, (uint32_t) 0xa54ff53au);
+        writeBigEnd(dest->bytes,4, (uint32_t) 0x510e527fu);
+        writeBigEnd(dest->bytes,5, (uint32_t) 0x9b05688cu);
+        writeBigEnd(dest->bytes,6, (uint32_t) 0x1f83d9abu);
+        writeBigEnd(dest->bytes,7, (uint32_t) 0x5be0cd19u);
+        return 1;
+}
+
+uint64_t computeHash(union Sha256 *dest,  const uint8_t *msg, 
+        const uint64_t msgBytes) 
+{
+        if (dest == NULL || msg == NULL || msgBytes >= UINT64_MAX / 8) {
+                return 0;
+        }
+
+        // Padding the message
+        const uint64_t padLen = paddedLen(msgBytes);
+        if (padLen == 0) {
+                // Should NEVER happen, but just in case
+                return 0;
+        }
+
+        uint8_t *padded = malloc(padLen);
+        if (padded == NULL || padMsg(padded, padLen, msg, msgBytes) == 0) {
+                free(padded);
+                return 0;
+        }
+
+        // Computing the hash over every 512 bit block
+        initSha256(dest);
+
+        //#region DEV START: Debugging our implementation
+        printf("Initial hash value: \n");
+        for (uint8_t i = 0; i < 8; i++) {
+                printf("  H[%d] = %08x\n", i, dest->words32[i]);
+        }
+        
+
+        printf("\nBlock Contents: \n");
+        for (uint8_t i = 0; i < 16; i++) {
+                printf("  W[%d] = %08x\n", i, readBigEnd(padded, i));
+        }
+        //#endregion DEV END
+
+        uint64_t ret = 0;
+        for (uint64_t i = 0; i < padLen; i += 64) {
+                if (computeBlockHash(dest, &padded[i]) == 0) {
+                        free(padded);
+                        return 0;
+                }
+                ret++;
+        }
+
+        free(padded);
+        return ret;
 }
--- src/hash.h
+++ src/hash.h
@@ -6,22 +6,26 @@
 #define HASH_H
 
 #include <stdint.h>
-#include <stdio.h>
+
+// 256-bit digest of a SHA-2 hash.
+union Sha256  {
+        uint8_t bytes[32];
+        uint32_t words32[8];
+};
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif /* NULL */
+
 
 // A SHA that is used as a placeholder. A warning should be issued if
 // a null SHA is detected and the --ignore-hash flag is not present. An
 // error must be thrown if a null sha collision is *generated*
-#define NULL_SHA { .bytes = { 0xE1u,0x6Bu,0x57u,0xA5u,0x88u,0x03u,0xE4u,0x3Cu,\
-                              0xBEu,0x51u,0x95u,0x8Fu,0x62u,0x08u,0x27u,0x58u,\
-                              0xA1u,0xA5u,0x0Du,0xA5u,0xCEu,0xF7u,0xE3u,0x88u,\
-                              0x69u,0x4Du,0xB8u,0xF1u,0x24u,0x3Fu,0x1Cu,0x40u } }
-
-
-// Little-endian 256-bit digest of a SHA-2 hash
-union Sha256 {
-        uint8_t bytes[32];
-        uint64_t longs[4];
-};
+#define NULL_SHA (union Sha256) { .bytes = {\
+        0xe1u,0x6bu,0x57u,0xa5u,0x88u,0x03u,0xe4u,0x3cu,\
+        0xbeu,0x51u,0x95u,0x8fu,0x62u,0x08u,0x27u,0x58u,\
+        0xa1u,0xa5u,0x0du,0xa5u,0xceu,0xf7u,0xe3u,0x88u,\
+        0x69u,0x4du,0xb8u,0xf1u,0x24u,0x3fu,0x1cu,0x40u } }
 
 // Gets the number of zeros that would be appended to a message of `msgBytes` 
 // octets.
@@ -50,8 +54,98 @@ uint64_t paddedLen(uint64_t msgBytes);
 // are ignored when
 //
 // outLen and msgBytes are measured in the number of bytes they store, not bits.
-// msg and outBuf will be stored in little endian format. 
 uint64_t padMsg(uint8_t *outBuf, uint64_t outBytes, const uint8_t *msg, 
         uint64_t msgBytes);
 
-#endif 
+// Shifts x right by n bits, and appends the n least significant bits to the
+// left of x (most significant). Behavior is undefined if n is 32 or more
+uint32_t rotr(uint32_t x, uint8_t n);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t ch(uint32_t x, uint32_t y, uint32_t z);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t maj(uint32_t x, uint32_t y, uint32_t z);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t bsig0(uint32_t x);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t bsig1(uint32_t x);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t ssig0(uint32_t x);
+
+// Logical bitwise function as specified by RFC-6234 5.1
+uint32_t ssig1(uint32_t x);
+
+// Constants for SHA-256 as specified by RFC 6234 5.1
+#define PRIME_CBRTS {\
+        0x428a2f98u, 0x71374491u, 0xb5c0fbcfu, 0xe9b5dba5u,\
+        0x3956c25bu, 0x59f111f1u, 0x923f82a4u, 0xab1c5ed5u,\
+        0xd807aa98u, 0x12835b01u, 0x243185beu, 0x550c7dc3u,\
+        0x72be5d74u, 0x80deb1feu, 0x9bdc06a7u, 0xc19bf174u,\
+        0xe49b69c1u, 0xefbe4786u, 0x0fc19dc6u, 0x240ca1ccu,\
+        0x2de92c6fu, 0x4a7484aau, 0x5cb0a9dcu, 0x76f988dau,\
+        0x983e5152u, 0xa831c66du, 0xb00327c8u, 0xbf597fc7u,\
+        0xc6e00bf3u, 0xd5a79147u, 0x06ca6351u, 0x14292967u,\
+        0x27b70a85u, 0x2e1b2138u, 0x4d2c6dfcu, 0x53380d13u,\
+        0x650a7354u, 0x766a0abbu, 0x81c2c92eu, 0x92722c85u,\
+        0xa2bfe8a1u, 0xa81a664bu, 0xc24b8b70u, 0xc76c51a3u,\
+        0xd192e819u, 0xd6990624u, 0xf40e3585u, 0x106aa070u,\
+        0x19a4c116u, 0x1e376c08u, 0x2748774cu, 0x34b0bcb5u,\
+        0x391c0cb3u, 0x4ed8aa4au, 0x5b9cca4fu, 0x682e6ff3u,\
+        0x748f82eeu, 0x78a5636fu, 0x84c87814u, 0x8cc70208u,\
+        0x90befffau, 0xa4506cebu, 0xbef9a3f7u, 0xc67178f2u }
+
+// Sets all data of the hash to the 32-bit words found in RFC 6234 6.1.
+// This returns 1 if no errors occur, and 0 otherwise. The only error
+// that can occur is if dest is NULL.
+uint8_t initSha256(union Sha256 *dest);
+
+// Computes a complete SHA-256 hash of the 512-bit block, outputting the
+// digest in big-endian format into the given destination pointer. All
+// existing data in said destination will be overridden. The existing data 
+// in the destination pointer will be used as the current value of the 
+// hash. 
+//
+// This functions returns 1 if the computation was successful, and 0 if an
+// error occurs. Any of the following can cause an error and return 0:
+//
+//  - block is NULL
+//  - destination is NULL
+//
+// The block is not padded by this, and is computed as-is.
+//
+// This function is derived from the steps given in RFC 6234 section 6.2
+uint8_t computeBlockHash(union Sha256 *destination, const uint8_t block[64]);
+
+// Computes a complete SHA-256 hash of a message, outputting the digest 
+// in big-endian format into the given destination pointer. All 
+// existing data in said destination will be overridden; the value of 
+// HASH_0 will be used as the initial hash, rather than any existing data
+// in the destination.
+//
+// The provided message is internally padded before any hashing occurs;
+// a consumer of this function need not (and should not) pad their message 
+// beforehand.
+//
+// This function returns the number of blocks that were computed, or 0
+// if an error occurred (even if something was computed). Any of the 
+// following can cause an error and return 0:
+//
+//  - msg is NULL
+//  - destination is NULL
+//  - msgBytes is too large (>= 2**61)
+//  - Memory could not be allocated for the padded message
+// 
+// If an error occurs, the data at destination is undefined.
+//
+// Please note that **msgBytes is measured in bytes.**, so a 256 bit 
+// message would provide msgBytes as equaling 32.
+//
+// This function is derived from the steps given in RFC 6234 section 6.2
+uint64_t computeHash(union Sha256 *destination, const uint8_t *msg, 
+        uint64_t msgBytes);
+
+#endif /* HASH_H */
